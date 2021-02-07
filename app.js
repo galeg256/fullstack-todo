b9e54469cc3d3afdc4745085913816ff3bdcb534
Egor <galkin_wp@mail.ru>
2021-02-07 21:03:31 +0500

    create fullstack-todo-app

---
+++ app.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import path from 'path'
+
+const app = express() 
+
+app.listen(5000, () => console.log('сервер запущен'))
---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render () {
+    console.log('frontend start')
+    return <h1>asd</h1>
+  }
+}
